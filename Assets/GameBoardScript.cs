f504c0143cd122bfe3918ee07a3a03d0d64014a6
Daniel Ng <ng17daniel@gmail.com>
2024-12-28 22:59:23 -0500

    initial commit

---
+++ Assets/GameBoardScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoardScript : MonoBehaviour
+{
+
+    [SerializeField] char[,] board = new char[8, 8];
+
+
+}
